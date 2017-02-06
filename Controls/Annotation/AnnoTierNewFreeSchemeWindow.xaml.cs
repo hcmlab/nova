2d61beb8935efa0ef10a7ab74e9f9c9bd81c2116
Tobias Baur <baur@hcm-lab.de>
2017-02-06 16:55:22 +0100

    refactored schemewindow

---
+++ Controls/Annotation/AnnoTierNewFreeSchemeWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace ssi
+{
+    public partial class AnnoTierNewFreeSchemeWindow : Window
+    {
+        public AnnoScheme Result { get; }
+
+        public AnnoTierNewFreeSchemeWindow()
+        {
+            InitializeComponent();
+
+            Result = new AnnoScheme();
+
+            backgroundColorPicker.SelectedColor = Colors.Black;
+            labelColorPicker.SelectedColor = Colors.LightYellow;
+            schemeNameTextBox.Text = "Noname";
+        }
+
+        private void okButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+
+            Result.MinOrBackColor = backgroundColorPicker.SelectedColor.Value;
+            Result.MaxOrForeColor = labelColorPicker.SelectedColor.Value;
+            Result.Name = schemeNameTextBox.Text == "" ? "Noname" : schemeNameTextBox.Text;
+
+            Close();
+        }
+
+        private void cancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+            Close();
+        }
+    }
+}
---
+++ Controls/Annotation/AnnoTierNewSchemeWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ssi
+{
+    /// <summary>
+    /// Interaktionslogik für NewTierWindow.xaml
+    /// </summary>
+    public partial class AnnoTierNewSchemeWindow : Window
+    {
+        AnnoScheme.TYPE annotype = AnnoScheme.TYPE.FREE;
+        public AnnoTierNewSchemeWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+            annotype = AnnoScheme.TYPE.FREE;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+            annotype = AnnoScheme.TYPE.DISCRETE;
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+            annotype = AnnoScheme.TYPE.CONTINUOUS;
+        }
+
+
+        public AnnoScheme.TYPE Result()
+        {
+            return annotype;
+        }
+
+    }
+}
