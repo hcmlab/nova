74b5c517480a3ffee82bad14a53f1a5ea4b43bc3
frankenjoe <frankenjoe@hotmail.com>
2017-03-28 16:17:27 +0200

    added missing files

---
+++ Controls/MainHandlerGeometric.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace ssi
+{
+    public partial class MainHandler
+    {
+
+        private void setPointList(PointList pl)
+        {
+            control.geometricListControl.geometricDataGrid.ItemsSource = pl;
+        }
+
+        private void geometricTableUpdate()
+        {
+            control.geometricListControl.geometricDataGrid.Items.Refresh();
+        }
+
+        private void geometricSelectItem(AnnoListItem item)
+        {
+            if (item.Points != null && item.Points.Count > 0)
+            {
+                setPointList(item.Points);              
+                geometricOverlayUpdate(item, AnnoScheme.TYPE.POINT);
+            }
+        }
+
+        public void geometricOverlayUpdate(AnnoListItem item, AnnoScheme.TYPE type)
+        {
+            WriteableBitmap overlay = null;
+
+            foreach (IMedia m in mediaList)
+            {
+                if (m.GetMediaType() == MediaType.VIDEO)
+                {
+                    overlay = m.GetOverlay();
+                    break;
+                }
+            }
+
+            if (overlay == null)
+            {
+                return;
+            }
+
+            overlay.Clear();
+
+            switch (type)
+            {
+                case AnnoScheme.TYPE.POINT:                            
+                    foreach (PointListItem p in item.Points)
+                    {
+                        if (p.XCoord != -1 && p.YCoord != -1)
+                        {
+                            Color color = item.Color;
+                            //color.A = 128;
+                            overlay.FillEllipseCentered((int)p.XCoord, (int)p.YCoord, 4, 4, color);
+                        }
+                    }
+                    break;
+                case AnnoScheme.TYPE.POLYGON:
+                    break;
+                case AnnoScheme.TYPE.GRPAH:
+                    break;
+                case AnnoScheme.TYPE.SEGMENTATION:
+                    break;
+            }
+        }
+
+        void OnMediaMouseDown(MediaKit media, double x, double y)
+        {
+            if (AnnoTierStatic.Selected != null &&
+                AnnoTierStatic.Selected.AnnoList.Scheme.Type == AnnoScheme.TYPE.POINT &&
+                control.annoListControl.annoDataGrid.SelectedItem != null &&
+                control.geometricListControl.geometricDataGrid.SelectedItem != null)
+            {
+                AnnoListItem item = (AnnoListItem)control.annoListControl.annoDataGrid.SelectedItem;
+                PointListItem point = (PointListItem)control.geometricListControl.geometricDataGrid.SelectedItem;
+                point.XCoord = x;
+                point.YCoord = y;
+                geometricTableUpdate();
+                geometricOverlayUpdate(item, AnnoScheme.TYPE.POINT);
+            }
+        }
+
+        private void geometricListEdit_Click(object sender, RoutedEventArgs e)
+        {
+            if (control.geometricListControl.geometricDataGrid.SelectedItems.Count == 1)
+            {
+                string name = control.geometricListControl.geometricDataGrid.SelectedItems[0].GetType().Name;
+                if (name == "PointListItem")
+                {
+                    foreach (PointListItem item in control.geometricListControl.geometricDataGrid.SelectedItems)
+                    {
+                        item.Label = control.geometricListControl.editTextBox.Text;
+                    }
+                }
+            }
+        }
+
+        private void geometricListEdit_Focused(object sender, MouseEventArgs e)
+        {
+            control.geometricListControl.editTextBox.SelectAll();
+        }
+
+        private void geometricListSelectAll_Click(object sender, RoutedEventArgs e)
+        {
+            if (control.geometricListControl.geometricDataGrid.Items.Count > 0)
+            {
+                control.geometricListControl.geometricDataGrid.SelectAll();
+            }
+        }
+
+        private void geometricListCopy_Click(object sender, RoutedEventArgs e)
+        {
+            if (control.annoListControl.annoDataGrid.SelectedItems.Count == 1)
+            {
+                if (control.geometricListControl.geometricDataGrid.Items[0].GetType().Name == "PointListItem")
+                {
+                    AnnoListItem item = (AnnoListItem)control.annoListControl.annoDataGrid.SelectedItems[0];
+                    AnnoList list = (AnnoList)control.annoListControl.annoDataGrid.ItemsSource;
+
+                    for (int i = 0; i < list.Count; ++i)
+                    {
+                        if (Math.Round(list[i].Start, 2) == Math.Round(item.Stop, 2))
+                        {
+                            for (int j = 0; j < list[i].Points.Count; ++j)
+                            {
+                                list[i].Points[j].Label = item.Points[j].Label;
+                                list[i].Points[j].XCoord = item.Points[j].XCoord;
+                                list[i].Points[j].YCoord = item.Points[j].YCoord;
+                            }
+                            break;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                MessageBoxResult mb = MessageBoxResult.OK;
+                mb = MessageBox.Show("Select one frame to copy", "Confirm", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+
+        private void geometricList_Selection(object sender, SelectionChangedEventArgs e)
+        {            
+            if (control.annoListControl.annoDataGrid.SelectedItem != null)
+            {
+                AnnoListItem item = (AnnoListItem) control.annoListControl.annoDataGrid.SelectedItem;
+                geometricOverlayUpdate(item, AnnoScheme.TYPE.POINT);
+            }
+
+            if (control.geometricListControl.geometricDataGrid.SelectedItems.Count == 1)
+            {
+                PointListItem item = (PointListItem)control.geometricListControl.geometricDataGrid.SelectedItems[0];
+                control.geometricListControl.editTextBox.Text = item.Label;
+            }
+        }
+
+        private void geometricListDelete(object sender, RoutedEventArgs e)
+        {
+            if (control.geometricListControl.geometricDataGrid.SelectedItems.Count != 0 
+                && control.annoListControl.annoDataGrid.SelectedItem != null)
+            {
+                AnnoListItem item = (AnnoListItem)control.annoListControl.annoDataGrid.SelectedItem;
+                foreach (PointListItem point in control.geometricListControl.geometricDataGrid.SelectedItems)
+                {
+                    point.XCoord = -1;
+                    point.YCoord = -1;
+                }
+                geometricTableUpdate();
+                geometricOverlayUpdate(item, AnnoScheme.TYPE.POINT);
+            }
+        }
+    }
+}
---
+++ Types/Skeleton.cs
@@ -0,0 +1,387 @@
+﻿using MediaToolkit;
+using MediaToolkit.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using System.Windows.Threading;
+using WPFMediaKit.DirectShow.Controls;
+
+namespace ssi
+{
+    public class Skeleton : Image, IMedia, INotifyPropertyChanged
+    {
+        private string filepath;
+        private Signal signal;
+        private MediaType type;
+
+        private Color signalColor;
+        private Color headColor;
+        private Color backColor;
+
+        private int width;
+        private int height;
+
+        private int numJoints = 0;
+        private int jointValues = 0;
+        private int numSkeletons = 1;
+
+        private WriteableBitmap writeableBmp;
+        private DispatcherTimer timer;
+        private List<Point3D> joints = new List<Point3D>();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged(string property)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            handler?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+
+        public Skeleton(string filepath, Signal signal, int width = 640, int height = 480)
+        {
+            this.filepath = filepath;
+            this.signal = signal;
+            this.width = width;
+            this.height = height;
+
+            type = MediaType.SKELETON;
+
+            BackColor = Defaults.Colors.Background;
+            SignalColor = Defaults.Colors.Foreground;
+            HeadColor = Defaults.Colors.Foreground;
+
+            numSkeletons = signal.meta_num;
+
+            if (signal.meta_type == "ssi") this.numJoints = 25;
+            else if (signal.meta_type == "kinect1") this.numJoints = 20;
+            else if (signal.meta_type == "kinect2") this.numJoints = 25;
+
+            jointValues = (int)((signal.dim / numSkeletons) / numJoints);
+
+            writeableBmp = new WriteableBitmap(width, height, 96.0, 96.0, PixelFormats.Bgr32, null);
+            writeableBmp.Clear(BackColor);            
+
+            Source = writeableBmp;
+            
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(1000.0 / signal.rate);
+            timer.Tick += new EventHandler(Draw);
+        }
+
+        public void Draw(object myObject, EventArgs myEventArgs)
+        {
+            Draw(MainHandler.Time.CurrentPlayPositionPrecise);
+        }
+
+        public void Draw(double time)
+        {            
+            int index = (int)(time * signal.rate);
+            
+            Color col = SignalColor;
+            Color colhead = HeadColor;
+
+            writeableBmp.Lock();
+            writeableBmp.Clear(BackColor);
+
+            if (index < signal.number)
+            {
+                Point[] points = new Point[numJoints];
+
+                //Kinect Stream Resolution
+                uint dim = signal.dim;
+
+                for (int s = 0; s < numSkeletons; s++)
+
+                {
+                    for (int i = 0; i < numJoints; i++)
+                    {
+                        if (index * dim < signal.data.Length)
+                        {
+                            if (signal.meta_type == "ssi")
+
+                            {
+                                points[i].X = (int)(signal.data[s * dim / numSkeletons + (index * dim) + i * jointValues + 0] * 75 / width + width / 2);
+                                points[i].Y = (int)(height - signal.data[s * dim / numSkeletons + (index * dim) + i * jointValues + 1] * 75 / height - height / 2);
+                            }
+                            else if (signal.meta_type == "kinect1")
+                            {
+                                points[i].X = (int)(signal.data[s * dim / numSkeletons + (index * dim) + i * jointValues + 0] * 75000 / width + width / 2);
+                                points[i].Y = (int)(height - signal.data[s * dim / numSkeletons + (index * dim) + i * jointValues + 1] * 75000 / height - height / 2);
+                            }
+                            else if (signal.meta_type == "kinect2")
+                            {
+                                points[i].X = (int)(signal.data[s * dim / numSkeletons + (index * dim) + i * jointValues + 0] * 75000 / width + width / 2);
+                                points[i].Y = (int)(height - signal.data[s * dim / numSkeletons + (index * dim) + i * jointValues + 1] * 75000 / height - height / 2);
+                            }
+                        }
+                    }
+
+                    if (signal.meta_type == "ssi")
+
+                    {
+                        //head
+                        writeableBmp.DrawLine((int)points[23].X, (int)points[23].Y, (int)points[21].X, (int)points[21].Y, colhead);
+                        writeableBmp.DrawLine((int)points[23].X, (int)points[23].Y, (int)points[22].X, (int)points[22].Y, colhead);
+                        writeableBmp.DrawLine((int)points[21].X, (int)points[21].Y, (int)points[24].X, (int)points[24].Y, colhead);
+                        writeableBmp.DrawLine((int)points[22].X, (int)points[22].Y, (int)points[24].X, (int)points[24].Y, colhead);
+
+                        writeableBmp.DrawLine((int)points[0].X, (int)points[0].Y, (int)points[1].X, (int)points[1].Y, getColor(1)); //Head to Neck
+
+                        writeableBmp.DrawLine((int)points[1].X, (int)points[1].Y, (int)points[4].X, (int)points[4].Y, getColor(4)); //Neck to Left  Shoulder
+                        writeableBmp.DrawLine((int)points[1].X, (int)points[1].Y, (int)points[8].X, (int)points[8].Y, getColor(8)); //Neck to Right Shoulder
+
+                        writeableBmp.DrawLine((int)points[4].X, (int)points[4].Y, (int)points[5].X, (int)points[5].Y, getColor(5)); // Left Shoulder to Left Elbow
+                        writeableBmp.DrawLine((int)points[8].X, (int)points[8].Y, (int)points[9].X, (int)points[9].Y, getColor(9)); //Rigtht Shoulder to Right Elbow
+
+                        writeableBmp.DrawLine((int)points[5].X, (int)points[5].Y, (int)points[6].X, (int)points[6].Y, getColor(6)); // Left Elbow to Left Wrist
+                        writeableBmp.DrawLine((int)points[9].X, (int)points[9].Y, (int)points[10].X, (int)points[10].Y, getColor(10)); //Rigth Elbow to Right Wrist
+
+                        writeableBmp.DrawLine((int)points[6].X, (int)points[6].Y, (int)points[7].X, (int)points[7].Y, getColor(7)); // Left Wrist to Left Hand
+                        writeableBmp.DrawLine((int)points[10].X, (int)points[10].Y, (int)points[11].X, (int)points[11].Y, getColor(11)); //Rigth Wrist to Right Hand
+
+                        writeableBmp.DrawLine((int)points[1].X, (int)points[1].Y, (int)points[2].X, (int)points[2].Y, getColor(2)); //Neck to Torso
+                        writeableBmp.DrawLine((int)points[2].X, (int)points[2].Y, (int)points[3].X, (int)points[3].Y, getColor(3)); //Torso to Hip Center
+
+                        writeableBmp.DrawLine((int)points[3].X, (int)points[3].Y, (int)points[12].X, (int)points[12].Y, getColor(12)); // Hip Center to Left Hip
+                        writeableBmp.DrawLine((int)points[3].X, (int)points[3].Y, (int)points[16].X, (int)points[16].Y, getColor(16)); //Hip Center to Right Hip
+
+                        writeableBmp.DrawLine((int)points[12].X, (int)points[12].Y, (int)points[13].X, (int)points[13].Y, getColor(13)); // Left Hip to Knee
+                        writeableBmp.DrawLine((int)points[16].X, (int)points[16].Y, (int)points[17].X, (int)points[17].Y, getColor(17)); // Right Hip to Knee
+
+                        writeableBmp.DrawLine((int)points[13].X, (int)points[13].Y, (int)points[14].X, (int)points[14].Y, getColor(14)); // Left Knee to Ankle
+                        writeableBmp.DrawLine((int)points[17].X, (int)points[17].Y, (int)points[18].X, (int)points[18].Y, getColor(18)); // Right Knee to Ankle
+
+                        writeableBmp.DrawLine((int)points[14].X, (int)points[14].Y, (int)points[15].X, (int)points[15].Y, getColor(15)); // Left Ankle to foot
+                        writeableBmp.DrawLine((int)points[18].X, (int)points[18].Y, (int)points[19].X, (int)points[19].Y, getColor(19)); // Right Ankle to foot
+                    }
+                    else if (signal.meta_type == "kinect1")
+
+                    {
+                        writeableBmp.DrawLine((int)points[3].X, (int)points[3].Y, (int)points[2].X, (int)points[2].Y, getColor(2)); //Head to Neck
+                        writeableBmp.DrawLine((int)points[2].X, (int)points[2].Y, (int)points[4].X, (int)points[4].Y, getColor(4)); //Neck to LeftShoulder
+                        writeableBmp.DrawLine((int)points[2].X, (int)points[2].Y, (int)points[8].X, (int)points[8].Y, getColor(8)); //Neck to RightShoulder
+                        writeableBmp.DrawLine((int)points[2].X, (int)points[2].Y, (int)points[1].X, (int)points[1].Y, getColor(1)); //Neck to Spine
+                        writeableBmp.DrawLine((int)points[1].X, (int)points[1].Y, (int)points[0].X, (int)points[0].Y, getColor(0)); //Spine to HipCenter
+
+                        writeableBmp.DrawLine((int)points[0].X, (int)points[0].Y, (int)points[12].X, (int)points[12].Y, getColor(12)); //HipCenter to HipLeft
+                        writeableBmp.DrawLine((int)points[0].X, (int)points[0].Y, (int)points[16].X, (int)points[16].Y, getColor(16)); //HipCenter to HipRight
+
+                        writeableBmp.DrawLine((int)points[4].X, (int)points[4].Y, (int)points[5].X, (int)points[5].Y, getColor(5)); //LeftShoulder to LeftElbow
+                        writeableBmp.DrawLine((int)points[5].X, (int)points[5].Y, (int)points[6].X, (int)points[6].Y, getColor(6)); //LeftElbow to LeftWrist
+                        writeableBmp.DrawLine((int)points[6].X, (int)points[6].Y, (int)points[7].X, (int)points[7].Y, getColor(7)); //LeftWrist to LeftHand
+
+                        writeableBmp.DrawLine((int)points[8].X, (int)points[8].Y, (int)points[9].X, (int)points[9].Y, getColor(9)); //RightShoulder to RightElbow
+                        writeableBmp.DrawLine((int)points[9].X, (int)points[9].Y, (int)points[10].X, (int)points[10].Y, getColor(10)); //RightElbow to RightWrist
+                        writeableBmp.DrawLine((int)points[10].X, (int)points[10].Y, (int)points[11].X, (int)points[11].Y, getColor(11)); //RightWrist to RightHand
+
+                        writeableBmp.DrawLine((int)points[12].X, (int)points[12].Y, (int)points[13].X, (int)points[13].Y, getColor(13)); //LeftHip to LeftKnee
+                        writeableBmp.DrawLine((int)points[13].X, (int)points[13].Y, (int)points[14].X, (int)points[14].Y, getColor(14)); //LeftKnee to LeftAnkle
+                        writeableBmp.DrawLine((int)points[14].X, (int)points[14].Y, (int)points[15].X, (int)points[15].Y, getColor(15)); //LeftAnkle to LeftFoot
+
+                        writeableBmp.DrawLine((int)points[16].X, (int)points[16].Y, (int)points[17].X, (int)points[17].Y, getColor(17)); //RightHip to RightKnee
+                        writeableBmp.DrawLine((int)points[17].X, (int)points[17].Y, (int)points[18].X, (int)points[18].Y, getColor(18)); //RightKnee to RightAnkle
+                        writeableBmp.DrawLine((int)points[18].X, (int)points[18].Y, (int)points[19].X, (int)points[19].Y, getColor(19)); //RightAnkle to RightFoot
+                    }
+                    else if (signal.meta_type == "kinect2")
+
+                    {
+                        writeableBmp.DrawLine((int)points[3].X, (int)points[3].Y, (int)points[2].X, (int)points[2].Y, getColor(2)); //Head to Neck
+                        writeableBmp.DrawLine((int)points[2].X, (int)points[2].Y, (int)points[20].X, (int)points[20].Y, getColor(20)); //Neck to Shoulder Spine
+
+                        writeableBmp.DrawLine((int)points[20].X, (int)points[20].Y, (int)points[4].X, (int)points[4].Y, getColor(4)); //Neck to Left  Shoulder
+                        writeableBmp.DrawLine((int)points[20].X, (int)points[20].Y, (int)points[8].X, (int)points[8].Y, getColor(8)); //Neck to Right Shoulder
+
+                        writeableBmp.DrawLine((int)points[4].X, (int)points[4].Y, (int)points[5].X, (int)points[5].Y, getColor(5)); // Left Shoulder to Left Elbow
+                        writeableBmp.DrawLine((int)points[8].X, (int)points[8].Y, (int)points[9].X, (int)points[9].Y, getColor(9)); //Rigtht Shoulder to Right Elbow
+
+                        writeableBmp.DrawLine((int)points[5].X, (int)points[5].Y, (int)points[6].X, (int)points[6].Y, getColor(6)); // Left Elbow to Left Wrist
+                        writeableBmp.DrawLine((int)points[9].X, (int)points[9].Y, (int)points[10].X, (int)points[10].Y, getColor(10)); //Rigth Elbow to Right Wrist
+
+                        writeableBmp.DrawLine((int)points[6].X, (int)points[6].Y, (int)points[7].X, (int)points[7].Y, getColor(7)); // Left Wrist to Left Hand
+                        writeableBmp.DrawLine((int)points[10].X, (int)points[10].Y, (int)points[11].X, (int)points[11].Y, getColor(11)); //Rigth Wrist to Right Hand
+
+                        writeableBmp.DrawLine((int)points[7].X, (int)points[7].Y, (int)points[21].X, (int)points[21].Y, getColor(21)); // Left Hand to Left Handtip
+                        writeableBmp.DrawLine((int)points[11].X, (int)points[11].Y, (int)points[23].X, (int)points[23].Y, getColor(23)); //Rigth Hand to Right Handtip
+
+                        writeableBmp.DrawLine((int)points[7].X, (int)points[7].Y, (int)points[22].X, (int)points[22].Y, getColor(22)); // Left Hand to Left thumb
+                        writeableBmp.DrawLine((int)points[11].X, (int)points[11].Y, (int)points[24].X, (int)points[24].Y, getColor(24)); //Rigth Hand to Right thumb
+
+                        writeableBmp.DrawLine((int)points[20].X, (int)points[20].Y, (int)points[1].X, (int)points[1].Y, getColor(1)); //Shoulder Spine to Spine
+                        writeableBmp.DrawLine((int)points[1].X, (int)points[1].Y, (int)points[0].X, (int)points[0].Y, getColor(0)); //Torso to Hip Center
+
+                        writeableBmp.DrawLine((int)points[0].X, (int)points[0].Y, (int)points[12].X, (int)points[12].Y, getColor(12)); // Hip Center to Left Hip
+                        writeableBmp.DrawLine((int)points[0].X, (int)points[0].Y, (int)points[16].X, (int)points[16].Y, getColor(16)); //Hip Center to Right Hip
+
+                        writeableBmp.DrawLine((int)points[12].X, (int)points[12].Y, (int)points[13].X, (int)points[13].Y, getColor(13)); // Left Hip to Knee
+                        writeableBmp.DrawLine((int)points[16].X, (int)points[16].Y, (int)points[17].X, (int)points[17].Y, getColor(17)); // Right Hip to Knee
+
+                        writeableBmp.DrawLine((int)points[13].X, (int)points[13].Y, (int)points[14].X, (int)points[14].Y, getColor(14)); // Left Knee to Ankle
+                        writeableBmp.DrawLine((int)points[17].X, (int)points[17].Y, (int)points[18].X, (int)points[18].Y, getColor(18)); // Right Knee to Ankle
+
+                        writeableBmp.DrawLine((int)points[14].X, (int)points[14].Y, (int)points[15].X, (int)points[15].Y, getColor(15)); // Left Ankle to foot
+                        writeableBmp.DrawLine((int)points[18].X, (int)points[18].Y, (int)points[19].X, (int)points[19].Y, getColor(19)); // Right Ankle to foot
+                    }
+                }
+            }
+
+            writeableBmp.Unlock();
+        }
+
+        private Color getColor(int i)
+        {
+            Color c = SignalColor;
+            double pos = MainHandler.Time.CurrentPlayPosition;
+            int index = (int)(pos * signal.rate);
+
+            if (signal.data.Length >= (index * signal.dim) + i * jointValues + 3)
+
+            {
+                if ((signal.data[(index * signal.dim) + i * jointValues + 3]) < 0.5)
+                {
+                    c = SignalColor;
+                    c.A = 128;
+                }
+                else if ((signal.data[(index * signal.dim) + i * jointValues + 3]) < 1.0)
+                {
+                    c = SignalColor;
+                    c.A = 0;
+                }
+                else
+                {
+                    c = SignalColor;
+                }
+            }
+            return c;
+        }
+
+        public MediaType GetMediaType()
+        {
+            return type;
+        }
+
+        public string GetFilepath()
+        {
+            return filepath;
+        }
+
+        public string GetDirectory()
+        {
+            return filepath.Substring(0, filepath.LastIndexOf("\\") + 1);
+        }
+
+        public bool HasAudio()
+        {
+            return false;
+        }
+
+        public void SetVolume(double volume)
+        {
+        }
+
+        public double GetVolume()
+        {
+            return 0;
+        }
+   
+        public void Move(double time)
+        {
+            Draw(time);
+        }
+
+        public double GetPosition()
+        {
+            return 0;
+        }
+
+        public double GetSampleRate()
+        {
+            return signal.rate;
+        }
+
+        public UIElement GetView()
+        {
+            return this;
+        }
+        public WriteableBitmap GetOverlay()
+        {
+            return writeableBmp;
+        }
+
+        public double GetLength()
+        {
+            return 0;
+        }
+
+        public void Clear()
+        {
+            if (writeableBmp != null)
+            {
+                writeableBmp.Clear(BackColor);
+            }
+        }
+
+        public void Play()
+        {
+            timer.Start();
+        }
+
+        public void Stop()
+        {
+            timer.Stop();
+        }
+
+        public void Pause()
+        {
+            timer.Stop();
+        }
+
+        public Color SignalColor
+        {
+            get
+            {
+                return signalColor;
+            }
+
+            set
+            {
+                signalColor = value;
+                RaisePropertyChanged("SignalColor");
+            }
+        }
+
+        public Color HeadColor
+        {
+            get
+            {
+                return headColor;
+            }
+
+            set
+            {
+                headColor = value;
+                RaisePropertyChanged("HeadColor");
+            }
+        }
+
+        public Color BackColor
+        {
+            get
+            {
+                return backColor;
+            }
+
+            set
+            {
+                backColor = value;
+                RaisePropertyChanged("BackColor");
+            }
+        }
+
+
+    }
+}
