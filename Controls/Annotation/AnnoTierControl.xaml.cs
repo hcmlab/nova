0a5c104f244d8197138ce7cef6dabfb71760925a
Tobias Baur <baur@hcm-lab.de>
2017-06-26 17:28:53 +0200

    minor ui fixes

--- Controls/Annotation/AnnoTierControl.xaml.cs
+++ Controls/Annotation/AnnoTierControl.xaml.cs
@@ -52,7 +52,7 @@ namespace ssi
             grid.Children.Add(tier);
 
             Label label = new Label();
-            label.Content = " " + tier.AnnoList.Meta.Role + " " + tier.AnnoList.Scheme.Name;
+            label.Content = " " + tier.AnnoList.Meta.Role + "." + tier.AnnoList.Scheme.Name;
             if (tier.AnnoList.Meta.AnnotatorFullName != "")
             {
                 label.Content += " (" + tier.AnnoList.Meta.AnnotatorFullName + ")";
