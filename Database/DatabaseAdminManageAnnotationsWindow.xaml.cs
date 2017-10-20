2922acb1cc8e036a34968593b656e0ebdb0b7a7a
Tobias Baur <baur@hcm-lab.de>
2017-10-20 14:02:55 +0200

    sort DB Annotation Main window, and DB Admin Manage Annotation Window

--- Database/DatabaseAdminManageAnnotationsWindow.xaml.cs
+++ Database/DatabaseAdminManageAnnotationsWindow.xaml.cs
@@ -2,10 +2,12 @@
 using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
 using System.Windows.Input;
 
 namespace ssi
@@ -15,6 +17,10 @@ namespace ssi
     /// </summary>
     public partial class DatabaseAdminManageAnnotationsWindow : Window
     {
+
+        GridViewColumnHeader _lastHeaderClicked = null;
+        ListSortDirection _lastDirection = ListSortDirection.Ascending;
+
         public DatabaseAdminManageAnnotationsWindow()
         {
             InitializeComponent();
@@ -162,5 +168,70 @@ namespace ssi
                 System.Windows.Data.CollectionViewSource.GetDefaultView(AnnotationsBox.ItemsSource).Refresh();
             }
         }
+
+
+        private void SortListView(object sender, RoutedEventArgs e)
+        {
+            GridViewColumnHeader headerClicked =
+             e.OriginalSource as GridViewColumnHeader;
+            ListSortDirection direction;
+
+            if (headerClicked != null)
+            {
+                if (headerClicked.Role != GridViewColumnHeaderRole.Padding)
+                {
+                    if (headerClicked != _lastHeaderClicked)
+                    {
+                        direction = ListSortDirection.Ascending;
+                    }
+                    else
+                    {
+                        if (_lastDirection == ListSortDirection.Ascending)
+                        {
+                            direction = ListSortDirection.Descending;
+                        }
+                        else
+                        {
+                            direction = ListSortDirection.Ascending;
+                        }
+                    }
+
+                    string header = headerClicked.Column.Header as string;
+                    ICollectionView dataView = CollectionViewSource.GetDefaultView(((ListView)sender).ItemsSource);
+
+                    dataView.SortDescriptions.Clear();
+                    SortDescription sd = new SortDescription(header, direction);
+                    dataView.SortDescriptions.Add(sd);
+                    dataView.Refresh();
+
+
+                    if (direction == ListSortDirection.Ascending)
+                    {
+                        headerClicked.Column.HeaderTemplate =
+                          Resources["HeaderTemplateArrowUp"] as DataTemplate;
+                    }
+                    else
+                    {
+                        headerClicked.Column.HeaderTemplate =
+                          Resources["HeaderTemplateArrowDown"] as DataTemplate;
+                    }
+
+                    // Remove arrow from previously sorted header  
+                    if (_lastHeaderClicked != null && _lastHeaderClicked != headerClicked)
+                    {
+                        _lastHeaderClicked.Column.HeaderTemplate = null;
+                    }
+
+                    _lastHeaderClicked = headerClicked;
+                    _lastDirection = direction;
+                }
+            }
+        }
+
+        private void AnnotationsBox_Click(object sender, RoutedEventArgs e)
+        {
+            SortListView(sender, e);
+        }
+
     }
 }
