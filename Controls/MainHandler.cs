2a3fd4f363c823a4db1b282cec4a5d9c529640fe
Tobias Baur <baur@hcm-lab.de>
2018-05-09 13:59:40 +0200

    now differentiates between train and predict annotator

--- Controls/MainHandler.cs
+++ Controls/MainHandler.cs
@@ -15,7 +15,7 @@ namespace ssi
     {
 
         //Config
-        public static string BuildVersion = "1.0.2.5";
+        public static string BuildVersion = "1.0.2.6";
         public static MEDIABACKEND MediaBackend = MEDIABACKEND.MEDIAKIT;
 
         private static Timeline timeline = null;
