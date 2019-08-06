7a001cfcdf181351cfc128ba174a74ca8d4dbc03
Tobias Baur <baur@hcm-lab.de>
2019-08-06 13:40:40 +0200

    download additional binaries

--- Controls/MainHandler.cs
+++ Controls/MainHandler.cs
@@ -16,7 +16,7 @@ namespace ssi
     {
 
         //Config
-        public static string BuildVersion = "1.0.5.9";
+        public static string BuildVersion = "1.0.6.0";
         public static MEDIABACKEND MediaBackend = MEDIABACKEND.MEDIAKIT;
         public static bool ENABLE_PYTHON = Properties.Settings.Default.EnablePython;
         public static int xaiProcessId;
