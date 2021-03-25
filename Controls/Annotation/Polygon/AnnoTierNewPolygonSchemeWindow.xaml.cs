c1a7b3b7a6081848ae8c92e4448168d27fde3bb7
Marco Steinberger <marco.steinberger@student.uni-augsburg.de>
2021-03-25 10:06:25 +0100

    Created the polygon scheme window and the mainControl part for the polygon case

---
+++ Controls/Annotation/Polygon/AnnoTierNewPolygonSchemeWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ssi
+{
+    /// <summary>
+    /// Interaction logic for AnnoTierNewPolygonSchemeWindow.xaml
+    /// </summary>
+    public partial class AnnoTierNewPolygonSchemeWindow : Window
+    {
+        private AnnoScheme scheme;
+
+        public AnnoTierNewPolygonSchemeWindow(ref AnnoScheme scheme)
+        {
+            InitializeComponent();
+
+            this.scheme = scheme;
+
+            nameTextBox.Text = scheme.Name;
+            srTextBox.Text = scheme.SampleRate.ToString();
+            dlTextBox.Text = scheme.DefaultLabel;
+        }
+
+        private void OkButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+
+            scheme.Name = nameTextBox.Text == "" ? Defaults.Strings.Unkown : nameTextBox.Text;
+            scheme.DefaultLabel = dlTextBox.Text == "" ? Defaults.Strings.Unkown : nameTextBox.Text;
+            
+            if (double.TryParse(srTextBox.Text, out double value))
+            {
+                scheme.SampleRate = value;
+            }
+
+            Close();
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+            Close();
+        }
+    }
+}
---
+++ Controls/Annotation/Polygon/PolygonListControl.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Media;
+using System.Collections;
+using System.Collections.ObjectModel;
+
+namespace ssi
+{
+    /// <summary>
+    /// Interaction logic for PolygonListControl.xaml
+    /// </summary>
+    public partial class PolygonListControl : UserControl
+    {
+        private GridViewColumnHeader listViewSortCol = null;
+        private ListViewSortAdorner listViewSortAdorner = null;
+
+        public PolygonListControl()
+        {
+            InitializeComponent();
+        }
+
+
+        private void editTextBox_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+        }
+
+        private void MenuItemSetConfidenceZeroClick(object sender, RoutedEventArgs e)
+        {
+            if (geometricDataGrid.SelectedItems.Count != 0)
+            {
+                string name = geometricDataGrid.SelectedItem.GetType().Name;
+                if (name == "PointListItem")
+                {
+                    foreach (PointListItem s in geometricDataGrid.SelectedItems)
+                    {
+                        s.Confidence = 0.0;
+                    }
+                }
+            }
+        }
+
+        private void MenuItemSetConfidenceOneClick(object sender, RoutedEventArgs e)
+        {
+            if (geometricDataGrid.SelectedItems.Count != 0)
+            {
+                string name = geometricDataGrid.SelectedItem.GetType().Name;
+                if (name == "PointListItem")
+                {
+                    foreach (PointListItem s in geometricDataGrid.SelectedItems)
+                    {
+                        s.Confidence = 1.0;
+                    }
+                }
+            }
+        }
+
+        private void GridViewColumnHeader_Click(object sender, RoutedEventArgs e)
+        {
+            GridViewColumnHeader column = (sender as GridViewColumnHeader);
+            if (column.Tag != null)
+            {                
+                string sortBy = column.Tag.ToString();
+                if (sortBy == "Label")
+                {
+
+                    if (listViewSortCol != null)
+                    {
+                        AdornerLayer.GetAdornerLayer(listViewSortCol).Remove(listViewSortAdorner);
+                        geometricDataGrid.Items.SortDescriptions.Clear();
+                    }
+
+                    if (listViewSortCol == null)
+                    {
+                        listViewSortCol = column;
+                        listViewSortAdorner = new ListViewSortAdorner(listViewSortCol, ListSortDirection.Ascending);
+                        AdornerLayer.GetAdornerLayer(listViewSortCol).Add(listViewSortAdorner);
+                        geometricDataGrid.Items.SortDescriptions.Add(new SortDescription(sortBy, ListSortDirection.Ascending));
+                    }
+                    else if (listViewSortAdorner.Direction == ListSortDirection.Ascending)
+                    {
+                        listViewSortAdorner = new ListViewSortAdorner(listViewSortCol, ListSortDirection.Descending);
+                        AdornerLayer.GetAdornerLayer(listViewSortCol).Add(listViewSortAdorner);
+                        geometricDataGrid.Items.SortDescriptions.Add(new SortDescription(sortBy, ListSortDirection.Descending));
+                    }
+                    else
+                    {
+                        listViewSortCol = null;
+                    }
+                }               
+            }     
+        }
+
+    }
+}
