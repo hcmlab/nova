046fb356d43d14484efe1208419ca0229f8537bc
Marco Steinberger <marco.steinberger@student.uni-augsburg.de>
2021-03-31 10:14:53 +0200

    Created the window to add a new polygon label

---
+++ Controls/Annotation/Polygon/AddPolygonLabelWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ssi
+{
+    /// <summary>
+    /// Interaction logic for AnnoTierNewPolygonSchemeWindow.xaml
+    /// </summary>
+    public partial class AddPolygonLabelWindow : Window
+    {
+        private AnnoScheme scheme;
+        private MainHandler mainHandler;
+
+        public AddPolygonLabelWindow(ref AnnoScheme scheme, ref MainHandler mainHandler)
+        {
+            InitializeComponent();
+
+            this.scheme = scheme;
+            this.mainHandler = mainHandler;
+            plTextBox.Text = scheme.DefaultLabel;
+            plTextBox.SelectAll();
+        }
+
+        private void OkButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+
+            PolygonLabel polygonLabel = new PolygonLabel(null, plTextBox.Text == "" ? Defaults.Strings.Unkown : plTextBox.Text);
+            this.mainHandler.addPolygonLabelToDataGrid(polygonLabel);
+            Close();
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+            Close();
+        }
+    }
+}
