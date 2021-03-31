eddf87b5b8b37fe3145790845afed26a4e164f29
Marco Steinberger <marco.steinberger@student.uni-augsburg.de>
2021-03-31 10:12:23 +0200

    Created new types for the polygon case

---
+++ Types/PolygonLabel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ssi
+{
+    public class PolygonLabel
+    {
+        private List<PolygonPoint> polygon;
+        private string label;
+
+        public PolygonLabel(List<PolygonPoint> polygon, string label)
+        {
+            this.polygon = polygon;
+            this.label = label;
+            
+        }        
+
+        public List<PolygonPoint> Polygon
+        {
+            get 
+            {
+                List<PolygonPoint> polygonClone = new List<PolygonPoint>();
+
+                foreach (PolygonPoint p in this.polygon)
+                    polygonClone.Add(new PolygonPoint(p.X, p.Y));
+
+                return polygonClone; 
+            }
+
+            set 
+            { 
+                this.polygon = value; 
+            }
+        }
+
+        public void addPoint(PolygonPoint p)
+        {
+            this.polygon.Add(p);
+        }
+
+        public void removeLastPoint()
+        {
+            this.polygon.RemoveAt(polygon.Count - 1);
+        }
+
+        public void removeAt(int pos)
+        {
+            this.polygon.RemoveAt(pos);
+        }
+
+        public string Label { get => this.label; set => this.label = value; }
+    }
+}
