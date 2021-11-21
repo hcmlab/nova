85106fe2f26ab0922dc1607461549ad665903bfa
Marco Steinberger <marco.steinberger@student.uni-augsburg.de>
2021-11-21 17:00:13 +0100

    Python Dateien fuer das lokale (nicht aus einer db heraus) Beispiel

---
+++ PythonScripts/polygon_network_example/UNetLikeNetwork.py
@@ -0,0 +1,51 @@
+from tensorflow import keras
+from tensorflow.keras import layers
+
+
+class UNetLikeNetwork:
+
+    def __init__(self, img_size, num_classes):
+        inputs = keras.Input(shape=img_size + (3,))
+
+        x = layers.Conv2D(32, 3, strides=2, padding="same")(inputs)
+        x = layers.BatchNormalization()(x)
+        x = layers.Activation("relu")(x)
+
+        previous_block_activation = x
+
+        for filters in [64, 128, 256]:
+            x = layers.Activation("relu")(x)
+            x = layers.SeparableConv2D(filters, 3, padding="same")(x)
+            x = layers.BatchNormalization()(x)
+
+            x = layers.Activation("relu")(x)
+            x = layers.SeparableConv2D(filters, 3, padding="same")(x)
+            x = layers.BatchNormalization()(x)
+
+            x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
+
+            residual = layers.Conv2D(filters, 1, strides=2, padding="same")(previous_block_activation)
+            x = layers.add([x, residual])  # Add back residual
+            previous_block_activation = x  # Set aside next residual
+
+        for filters in [256, 128, 64, 32]:
+            x = layers.Activation("relu")(x)
+            x = layers.Conv2DTranspose(filters, 3, padding="same")(x)
+            x = layers.BatchNormalization()(x)
+
+            x = layers.Activation("relu")(x)
+            x = layers.Conv2DTranspose(filters, 3, padding="same")(x)
+            x = layers.BatchNormalization()(x)
+
+            x = layers.UpSampling2D(2)(x)
+
+            residual = layers.UpSampling2D(2)(previous_block_activation)
+            residual = layers.Conv2D(filters, 1, padding="same")(residual)
+            x = layers.add([x, residual])  # Add back residual
+            previous_block_activation = x  # Set aside next residual
+
+        outputs = layers.Conv2D(num_classes, 3, activation="softmax", padding="same")(x)
+        self.model = keras.Model(inputs, outputs)
+
+    def get_model(self):
+        return self.model
