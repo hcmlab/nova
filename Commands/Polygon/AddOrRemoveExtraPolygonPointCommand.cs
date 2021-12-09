9f29a17a03e844411d92a0a4f86837ae9069881c
Marco Steinberger <marco.steinberger@student.uni-augsburg.de>
2021-12-09 09:16:53 +0100

    Kommands fuer Undo-Redo

---
+++ Commands/Polygon/AddOrRemoveExtraPolygonPointCommand.cs
@@ -0,0 +1,37 @@
+﻿using ssi.Interfaces;
+using System.Collections.Generic;
+using System.Windows;
+using static ssi.Types.Polygon.LabelInformations;
+
+namespace ssi.Types.Polygon
+{
+    class AddOrRemoveExtraPolygonPointCommand : ICommand
+    {
+        private PolygonLabel polygonToChange;
+        private double lastID;
+        private PolygonPoint newPoint;
+        private TYPE type;
+
+        public AddOrRemoveExtraPolygonPointCommand(double ID, PolygonPoint point, PolygonLabel polygonToChange, TYPE type)
+        {
+            this.polygonToChange = polygonToChange;
+            this.lastID = ID;
+            this.newPoint = point;
+            this.type = type;
+        }
+
+        public PolygonLabel Do()
+        {
+            polygonToChange.addPointAfterSpecificID(this.lastID, this.newPoint);
+            polygonToChange.Informations = new LabelInformations(this.type);
+            return polygonToChange;
+        }
+
+        public PolygonLabel Undo()
+        {
+            polygonToChange.removePointAfterSpecificID(this.lastID);
+            polygonToChange.Informations = new LabelInformations(this.type);
+            return polygonToChange;
+        }
+    }
+}
---
+++ Commands/Polygon/ChangeCompletePolygonCommand .cs
@@ -0,0 +1,51 @@
+﻿using ssi.Interfaces;
+using System.Collections.Generic;
+using System.Windows;
+using static ssi.Types.Polygon.LabelInformations;
+
+namespace ssi.Types.Polygon
+{
+    class ChangeCompletePolygonCommand : ICommand
+    {
+        private PolygonLabel polygonToChange;
+        private List<Point> oldPoints;
+        private List<Point> newPoints;
+        private TYPE type;
+
+        public ChangeCompletePolygonCommand(List<Point> oldPoints, List<Point> newPoints, PolygonLabel polygonToChange, TYPE type)
+        {
+            this.polygonToChange = polygonToChange;
+            this.oldPoints = oldPoints;
+            this.newPoints = newPoints;
+            this.type = type;
+        }
+
+        public PolygonLabel Do()
+        {
+            int counter = 0;
+            foreach(PolygonPoint point in polygonToChange.Polygon)
+            {
+                point.X = newPoints[counter].X;
+                point.Y = newPoints[counter].Y;
+                counter++;
+            }
+
+            polygonToChange.Informations = new LabelInformations(this.type);
+            return polygonToChange;
+        }
+
+        public PolygonLabel Undo()
+        {
+            int counter = 0;
+            foreach (PolygonPoint point in polygonToChange.Polygon)
+            {
+                point.X = oldPoints[counter].X;
+                point.Y = oldPoints[counter].Y;
+                counter++;
+            }
+
+            polygonToChange.Informations = new LabelInformations(this.type);
+            return polygonToChange;
+        }
+    }
+}
