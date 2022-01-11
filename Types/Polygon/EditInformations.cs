0cc680169acbaa31ee9978bacdb7967c8685f8ec
Marco Steinberger <marco.steinberger@student.uni-augsburg.de>
2022-01-11 20:11:43 +0100

    Informationen als Variablen und zwei Methoden hinzugefuegt

--- Types/Polygon/EditInformations.cs
+++ Types/Polygon/EditInformations.cs
@@ -10,34 +10,31 @@ namespace ssi.Types.Polygon
 {
     class EditInformations
     {
-        private bool isEditModeOn = false;
         private bool mouseOnMedia = false;
         private bool showingSelectionRect = false;
-        private bool isAboveSelectedPolygonPoint = false;
-        private bool isWithinSelectedPolygonArea = false;
+        private PolygonPoint selectedPoint = null;
+        private PolygonLabel selectedPolygon = null;
         private bool isAboveSelectedPolygonLineAndCtrlPressed = false;
-        private PolygonPoint selectedPolygonPoint;
         private Point startPosition = new Point(-1, -1);
         private List<Point> polygonStartPosition;
+        private List<Tuple<double, double>> polygonPointsXandYDistances = new List<Tuple<double, double>>();
         private PolygonPoint lastPolygonPoint;
         private double imageWidth = 0;
         private double imageHeight = 0;
 
-
-        public bool IsEditModeOn { get => isEditModeOn; set => isEditModeOn = value; }
         public bool MouseOnMedia { get => mouseOnMedia; set => mouseOnMedia = value; }
         public bool ShowingSelectionRect { get => showingSelectionRect; set => showingSelectionRect = value; }
 
-        public bool IsWithinSelectedPolygonArea { get => isWithinSelectedPolygonArea; set => isWithinSelectedPolygonArea = value; }
-        public bool IsAboveSelectedPolygonPoint { get => isAboveSelectedPolygonPoint; set => isAboveSelectedPolygonPoint = value; }
+        public PolygonLabel SelectedPolygon { get => selectedPolygon; set => selectedPolygon = value; }
+        public PolygonPoint SelectedPoint { get => selectedPoint; set => selectedPoint = value; }
         public bool IsAboveSelectedPolygonLineAndCtrlPressed { get => isAboveSelectedPolygonLineAndCtrlPressed; set => isAboveSelectedPolygonLineAndCtrlPressed = value; }
 
         public Point StartPosition { get => startPosition; set => startPosition = value; }
         public List<Point> PolygonStartPosition { get => polygonStartPosition; set => polygonStartPosition = value; }
-        public PolygonPoint SelectedPolygonPoint { get => selectedPolygonPoint; set => selectedPolygonPoint = value; }
         public PolygonPoint LastPolygonPoint { get => lastPolygonPoint; set => lastPolygonPoint = value; }
         public double ImageWidth { get => imageWidth; set => imageWidth = value; }
         public double ImageHeight { get => imageHeight; set => imageHeight = value; }
+        public List<Tuple<double, double>> PolygonPointsXandYDistances { get => polygonPointsXandYDistances; set => polygonPointsXandYDistances = value; }
 
         public EditInformations()
         {
@@ -48,9 +45,20 @@ namespace ssi.Types.Polygon
         {
             startPosition = new Point(-1, -1);
             isAboveSelectedPolygonLineAndCtrlPressed = false;
-            isAboveSelectedPolygonPoint = false;
-            isWithinSelectedPolygonArea = false;
-            selectedPolygonPoint = null;
+            SelectedPoint = null;
+            selectedPolygon = null;
+        }
+
+        public void setSelectedPolygonPointsAsDistanceToMouse(List<Point> polygon, double xMouse, double yMouse)
+        {
+            polygonPointsXandYDistances = new List<Tuple<double, double>>();
+            polygon.ForEach(point => polygonPointsXandYDistances.Add(new Tuple<double, double>(point.X - xMouse, point.Y - yMouse)));
+        }
+
+        public void resetSelectedElements()
+        {
+            SelectedPoint = null;
+            selectedPolygon = null;
         }
     }
 }
