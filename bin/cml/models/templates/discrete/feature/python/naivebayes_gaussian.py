0dae247408881b0840c3f6ef7ee1f100c4b11899
Tobias Baur <tobias.baur@uni-a.de>
2022-12-13 22:36:40 +0100

    Update naivebayes_gaussian.py

--- bin/cml/models/templates/discrete/feature/python/naivebayes_gaussian.py
+++ bin/cml/models/templates/discrete/feature/python/naivebayes_gaussian.py
@@ -11,17 +11,41 @@ def train (X, Y):
     print('train_x shape: {} | train_x[0] shape: {}'.format(X.shape, X[0].shape))
     model.fit(X, Y) 
     return model
-    
-def save (model, path):
-    with open(path + '.model', 'wb') as f:
-        pickle.dump(model, f)
-        print('Model stored at: {}'.format(path))
 
-def predict (model, X):
-    Y = model.predict(X)
+def save (model, path, logger=None):
+    if not Path(path.parent).is_dir():
+        path.parent.mkdir(parents=True, exist_ok=True)
+        # out_path = str(path) + ".pth"
+    out_path = str(path) + MODEL_SUFFIX
+    with open(out_path, 'wb') as f:
+        pickle.dump(model, f)
+        #print('Model stored at: {}'.format(path))
+    return out_path
+
+
+def predict (model, data, logger=None):
+    Y = model.predict(data[0])
     return Y
 
 
-def load (path):
-    model = pickle.load( open( path, "rb" ))
+def load (path, classes, logger=None):
+    file = open(path,'rb')
+    model = pickle.load(file)
+    #model = pickle.load(path, "rb" )
     return model
+
+def preprocess(ds_iter, request_form=[], logger=None):
+    data_list = list(ds_iter)
+    data_list.sort(key=lambda x: int(x["frame"].split("_")[0]))
+
+    # Get keys for all roles
+
+    x = []
+    y = []
+    for r in request_form["roles"].split(';'):
+        x_tmp = [v[r + '.' + request_form["streamName"].split(" ")[0]] for v in data_list]
+        y_tmp = [v[r + '.' + request_form["scheme"].split(";")[0]] for v in data_list]
+        x.extend(x_tmp)
+        y.extend(y_tmp)
+
+    return np.ma.concatenate(x, axis=0), np.array(y)
