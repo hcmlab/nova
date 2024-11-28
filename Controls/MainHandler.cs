302144cc2368beba94e7a570f2eb06b1a4aed996
Dr. Tobias Baur <tobiasbaur@users.noreply.github.com>
2024-11-28 12:45:44 +0100

    bump version to major release

--- Controls/MainHandler.cs
+++ Controls/MainHandler.cs
@@ -23,7 +23,7 @@ namespace ssi
     {
 
         //Config
-        public static string BuildVersion = "1.2.9.9";
+        public static string BuildVersion = "1.3.0.0";
         public static MEDIABACKEND MediaBackend = (Properties.Settings.Default.MediaBackend == "Hardware") ? MEDIABACKEND.MEDIAKIT : MEDIABACKEND.MEDIA;
         public static bool ENABLE_PYTHON = Properties.Settings.Default.EnablePython;
         public static bool ENABLE_LIGHTNING = Properties.Settings.Default.EnableLightning;
